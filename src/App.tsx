0f4ddeb1fee92a336d15b23c575d5843d1ae45ba
CLOUDYURI9 <yuridinato@gmail.com>
2025-03-19 16:31:46 -0300

    Criando a pagina inicial pelo React

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Cabecalho from './components/Cabecalho/Cabecalho'
+import Rodape from './components/Rodape/Rodape'
+import Principal from './components/Welcome/Welcome'
+
+function App() {
+  
+
+  return (
+    <>
+      <Cabecalho/>
+      <Principal/>
+      <Rodape/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Rodape/Rodape.tsx
@@ -0,0 +1,13 @@
+import estilo from'./Rodape.module.css';
+
+function Rodape() {
+    return(
+        <footer className={estilo.rodape}>
+            <p>Desenvolvido por:Yuri</p>
+            <p>Copyright</p>
+        </footer>
+    )
+
+}
+
+export default Rodape;
