a9b8ff33f954aede50874db808a585473980a9d6
CLOUDYURI9 <yuridinato@gmail.com>
2025-04-09 09:39:55 -0300

    copiando repositorio do Renato

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-
-import './App.css'
-import AppRoutes from './routes';
+import './App.css';
+import AppRoutes from './routes'; // importa o componente AppRoutes
 
 function App() {
-
-
   return (
     <>
       <AppRoutes />
@@ -12,4 +9,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
